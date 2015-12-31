1811d140e21400d7373cb619070971bf6266536b
FynnBorehamSmith <Fynn@boreham-smith.com>
2015-12-31 11:45:56 +1100

    Chapter 2 exercises

---
+++ chap2.js
@@ -0,0 +1,9 @@
+var secondsInAMinute = 60;
+var minutesInAHour = 60;
+var secondsInAHour = secondsInAMinute * minutesInAHour;
+var hoursInADay = 24;
+var secondsInADay = hoursInADay * secondsInAHour;
+var daysInAYear = 365;
+var secondsInAYear = secondsInADay * daysInAYear;
+var fynnsAge = 12;
+var dadsAge = 45;
